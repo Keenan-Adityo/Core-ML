bf896c8a7c2a8d2da8bdc00cf70718bdc1e67ec1
Keenan <keenanadityo167@gmail.com>
2025-04-10 20:55:00 +0700

    first commit

---
+++ Core-ML/Core_MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_MLApp.swift
+//  Core-ML
+//
+//  Created by Keenan Adityo on 10/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Core_MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
