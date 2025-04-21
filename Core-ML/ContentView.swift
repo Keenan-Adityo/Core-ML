4aa702cca9366df72b9f109d8934c113514b1891
Keenan <keenanadityo167@gmail.com>
2025-04-21 13:48:43 +0700

    add view

--- Core-ML/ContentView.swift
+++ Core-ML/ContentView.swift
@@ -9,14 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack() {
+            VStack {
+                Text("Alat scan uang!")
+            }
+            Spacer() // kamera
+            VStack {
+                Text("Jumlah Uang dalam Dompet")
+                    .background(.yellow)
+                Button(action: {
+                       //do action
+                }) {
+                    Text("Masukkan ke dompet")
+                        .frame(width: 200 , height: 50, alignment: .center)
+                        //You need to change height & width as per your requirement
+                }
+                .frame(maxWidth: .infinity)
+                 .background(Color.blue)
+                 .foregroundColor(Color.white)
+                 .cornerRadius(5)
+                Button(action: {
+                       //do action
+                }) {
+                    Text("Keluarkan dari dompet")
+                        .frame(width: 200 , height: 50, alignment: .center)
+                        //You need to change height & width as per your requirement
+                }
+                .frame(maxWidth: .infinity)
+                .background(Color.green)
+                .foregroundColor(Color.white)
+                .cornerRadius(5)
+            }
+            HStack {
+                
+            }
         }
         .padding()
+        
     }
+    
 }
 
 #Preview {
